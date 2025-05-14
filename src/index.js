4e1beffec06fbd6c3dd780afecec382f77ef3e71
Ifham-Hashir <ifham09052003@gmail.com>
2025-05-14 22:47:22 +0530

    Display temp in the console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './styles/reset.css';
 import './styles/global.css';
 
+const location = document.querySelector("#location");
+const searchBtn = document.querySelector("#search-btn");
+
+searchBtn.addEventListener("click", () => {
+  fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.value}?key=NPNMUE3L6YWN2B95NY3CFQDG3`, {mode: 'cors'})
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.currentConditions.temp);
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log("Error");
+  })
+});
